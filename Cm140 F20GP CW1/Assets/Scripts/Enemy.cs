0d3deffaa69db18fb0a3721d92de5c785a4a01f0
CalumMcAdam99 <CalumMcAdam15@outlook.com>
2021-03-04 00:06:34 +0000

    commit

---
+++ Cm140 F20GP CW1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    private Rigidbody2D rb;
+    public GameObject target;
+    float moveSpeed;
+    private Vector3 directionToTarget;
+
+    void Start()
+    {
+        //sets player as target
+        target = GameObject.Find("Char");
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = Random.Range(1f, 3f);
+
+    }
+
+    void Update()
+    {
+
+        MoveEnemy();
+
+    }
+  
+    void MoveEnemy()
+    {
+        if (target != null)
+        {
+            //moves towards player if target is selected
+            directionToTarget = (target.transform.position - transform.position).normalized;
+            rb.velocity = new Vector2(directionToTarget.x * moveSpeed,
+                                        directionToTarget.y * moveSpeed);
+        }
+        else
+            rb.velocity = Vector3.zero;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.gameObject.tag)
+        {
+
+            //check for player collision
+            case "Player":
+                DeployEnemies.spawnAllowed = false;
+
+                Destroy(other.gameObject);
+                target = null;
+
+                break;
+
+                //check for fireball collision
+            case "Fire":
+                ScoreScript.scoreVal += 10; 
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+                break;
+        }
+    }
+}
---
+++ Cm140 F20GP CW1/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    public GameObject Fireball;
+    public GameObject crosshairs;
+    private Vector3 target;
+    public GameObject player;
+
+    public float speed = 15.0f;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshairs.transform.position = new Vector2(target.x, target.y);
+
+       Vector3 Difference = target - player.transform.position;
+       float rotationZ = Mathf.Atan2(Difference.y, Difference.x) * Mathf.Rad2Deg;
+       player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+        //Firing 
+        if (Input.GetMouseButtonDown(0))
+        {
+            float distance = Difference.magnitude;
+            Vector2 direction = Difference / distance;
+            direction.Normalize();
+            fire(direction, rotationZ);
+        }
+       
+
+    }
+    //Creating Fired object and moving it
+    void fire(Vector2 direction, float rotationZ)
+    {
+        GameObject b = Instantiate(Fireball) as GameObject;
+        b.transform.position = player.transform.position;
+        b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        b.GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
+
